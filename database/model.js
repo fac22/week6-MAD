b95e20c52e5cdb589cd60f4543df5da228b77803
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-11-02 17:48:33 +0000

    part way through product pages
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: Danilo <81513148+danilo-cupido@users.noreply.github.com>

--- database/model.js
+++ database/model.js
@@ -7,4 +7,11 @@ function getProducts() {
   return db.query(SELECT_PRODUCTS).then((result) => result.rows);
 }
 
-export { getProducts };
+function getProduct(id) {
+  const SELECT_PRODUCT = /* sql */ `
+  SELECT name, genre, category, publisher, price, stock, to_char(created_at, 'DD Mon YYYY') AS created_at FROM products WHERE id=$1
+  `;
+  return db.query(SELECT_PRODUCT, [id]).then((result) => result.rows[0]);
+}
+
+export { getProducts, getProduct };
