70514bd1d3ec77aaacd0573af10be942dc7079cb
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-11-03 15:26:25 +0000

    filled product descriptions and updated product db table
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: Danilo <81513148+danilo-cupido@users.noreply.github.com>

--- database/model.js
+++ database/model.js
@@ -2,14 +2,14 @@ import db from './connection';
 
 function getProducts() {
   const SELECT_PRODUCTS = /* sql */ `
-  SELECT id, name, genre, category, publisher, price, stock, to_char(created_at, 'DD Mon YYYY') AS created_at FROM products
+  SELECT id, name, description, genre, category, publisher, price, stock, to_char(created_at, 'DD Mon YYYY') AS created_at FROM products
   `;
   return db.query(SELECT_PRODUCTS).then((result) => result.rows);
 }
 
 function getProduct(name) {
   const SELECT_PRODUCT = /* sql */ `
-  SELECT name, genre, category, publisher, price, stock, to_char(created_at, 'DD Mon YYYY') AS created_at FROM products WHERE name=$1
+  SELECT id, name, description, genre, category, publisher, price, stock, to_char(created_at, 'DD Mon YYYY') AS created_at FROM products WHERE name=$1
   `;
   return db.query(SELECT_PRODUCT, [name]).then((result) => result.rows[0]);
 }
