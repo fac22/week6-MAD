accd5e4a8e2bd62575675b8bad296c6fe9939592
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-11-04 14:58:24 +0000

    no items message
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: Danilo <81513148+danilo-cupido@users.noreply.github.com>

--- components/ProductList.js
+++ components/ProductList.js
@@ -1,31 +1,38 @@
 import Link from 'next/dist/client/link';
 
-
-export default function ProductList({ products, min, max, genre, publisher, category }) {
+export default function ProductList({
+  products,
+  min,
+  max,
+  genre,
+  publisher,
+  category,
+}) {
+  const listItems = products
+    .filter((product) =>
+      category === 'All' ? products : product.category === category
+    )
+    .filter((product) =>
+      publisher === 'All' ? true : product.publisher === publisher
+    )
+    .filter((product) => (genre === 'All' ? products : product.genre === genre))
+    .filter(
+      (product) =>
+        parseInt(product.price) >= min && parseInt(product.price) <= max
+    )
+    .map((product) => {
+      return (
+        <li key={product.id}>
+          <Link href={`/products/${product.name}`}>
+            <a>{product.name}</a>
+          </Link>
+          , £{product.price}
+        </li>
+      );
+    });
   return (
     <ul>
-      {products
-        .filter((product) =>
-          category === 'All' ? products : product.category === category
-        )
-        .filter((product) =>
-          publisher === 'All' ? true : product.publisher === publisher
-        )
-        .filter((product) => genre === 'All' ? products : product.genre === genre)
-        .filter(
-          (product) =>
-            parseInt(product.price) >= min && parseInt(product.price) <= max
-        )
-        .map((product) => {
-          return (
-            <li key={product.id}>
-              <Link href={`/products/${product.name}`}>
-                <a>{product.name}</a>
-              </Link>
-              , £{product.price}
-            </li>
-          );
-        })}
+      {listItems.length ? listItems : <li>No items match your filters!</li>}
     </ul>
   );
 }
