a355281ab82e7e20c06c7d87f537ac3bbb89114b
Danilo <81513148+danilo-cupido@users.noreply.github.com>
2021-11-04 17:15:17 +0000

    remove console.log

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import React from 'react';
 
 function MyApp({ Component, pageProps }) {
   const [basket, setBasket] = React.useState({});
-  console.log('app', basket);
   return <Component {...{ ...pageProps, basket, setBasket }} />;
 }
 
