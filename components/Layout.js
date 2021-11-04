6c777e5be26e43949f4b313bc466e25b9914d7fd
lopezelpesado <76691426+lopezelpesado@users.noreply.github.com>
2021-11-04 11:41:28 +0000

    added footer
    Co-authored-by: Michael <72146072+MJOW1999@users.noreply.github.com>
    Co-authored-by: Danilo <81513148+danilo-cupido@users.noreply.github.com>

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Layout({ children, home }) {
   return (
@@ -20,11 +21,31 @@ export default function Layout({ children, home }) {
         </section>
       </header>
       <main>{children}</main>
-      {!home && (
-        <Link href="/">
-          <a>Return Home</a>
-        </Link>
-      )}
+      <footer>
+        <address>
+          <p>MAD_GAMES</p>
+          <p>Space4</p>
+          <p>2nd Floor 113-115 Fonthill Road</p>
+          <p>Finsbury Park</p>
+          <p>London</p>
+          <p>N4 3HH</p>
+        </address>
+        <p>
+          <a href="https://github.com/fac22/week6-MAD">
+            <Image
+              src="/GitHub.png"
+              width="32"
+              height="32"
+              alt="GitHub logo of a silhouetted cat with an octopus tentacle, click to go to GitHub repo for this website."
+            />
+          </a>
+        </p>
+        <p>© 2021 MAD_GAMES. All rights reserved.</p>
+        <p>
+          MAD GAMES LTD. is a company registered in England and Wales (No.
+          12345678).
+        </p>
+      </footer>
     </>
   );
 }
